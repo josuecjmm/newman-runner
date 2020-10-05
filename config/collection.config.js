a1d30626c176d6994c3247e71f2390c602edabaa
connorturok27 <josuecjmm@gmail.com>
2020-10-04 22:01:32 -0600

    Add initial files to project

---
+++ config/collection.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    collection: 'https://www.getpostman.com/collections/baef31a128ff8913eb2d',
+    environment: require('../environments/PROD.json')
+}
---
+++ run-newman-collection.js
@@ -0,0 +1,41 @@
+const newman = require('newman')
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+
+const {collection, environment} = require('./config/collection.config'); 
+
+async function createNewmanDir() {
+  const { stdout, stderr } = await exec(
+    `cd ./reports
+    mkdir -p newman`
+  )
+  if (stdout !== '') console.log('stdout:', stdout)
+  if (stderr !== '') console.log('stdout:', stderr)
+}
+
+createNewmanDir()
+
+async function openReport() {
+  const { stdout, stderr } = await exec(
+    `cd ./reports/newman
+    file=$(ls -t | head -n1 |awk '{printf("%s",$0)}') 
+    open $file`
+  )
+  if (stdout !== '') console.log('stdout:', stdout)
+  if (stderr !== '') console.log('stdout:', stderr)
+}
+
+newman.run({
+  collection,
+  environment,
+  reporters: ['htmlextra', 'cli'],
+  reporter: {
+    htmlextra: {
+      export: './reports/newman',
+      logs: true,
+    },
+  },
+}, (err) => {
+  if (err) { throw err }
+  openReport()
+})
